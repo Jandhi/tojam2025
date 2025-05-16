8419071b1e164d59ed71455d49bbbcbb9a4e5da5
Rosalind Chapman <chapman.rosalind@gmail.com>
2025-05-16 14:09:58 -0400

    unit spawn list

--- combat/unit_manager.gd
+++ combat/unit_manager.gd
@@ -14,6 +14,7 @@ var units = [
 
 	preload("res://unit_data/hedgewitch.tscn"),
 	preload("res://unit_data/imp.tscn"),
+	preload("res://unit_data/night_court.tscn"),
 	preload("res://unit_data/night_court_archer.tscn"),
 	preload("res://unit_data/nun.tscn"),
 	preload("res://unit_data/sellswordarcher.tscn"),
@@ -21,10 +22,26 @@ var units = [
 	preload("res://unit_data/treeman.tscn"),
 	preload("res://unit_data/troll.tscn"),
 	preload("res://unit_data/zombie.tscn"),
+	preload("res://unit_data/zombie.tscn"),
+	preload("res://unit_data/skeleton.tscn"),
+	preload("res://unit_data/skeletonArcher.tscn"),
+	preload("res://unit_data/vampire.tscn"),
+	preload("res://unit_data/knight.tscn"),
+	preload("res://unit_data/boneMage.tscn"),
+	preload("res://unit_data/succubus.tscn"),
 ]
 
 var gold_units = [
-
+	preload("res://unit_data/battlesister.tscn"),
+	preload("res://unit_data/battlesisterArcher.tscn"),
+	preload("res://unit_data/brotherhood.tscn"),
+	preload("res://unit_data/brotherhoodArcher.tscn"),
+	preload("res://unit_data/churchman.tscn"),
+	preload("res://unit_data/sellswordarcher.tscn"),
+	preload("res://unit_data/sellswordm.tscn"),
+	preload("res://unit_data/skeleton.tscn"),
+	preload("res://unit_data/skeletonArcher.tscn"),
+	preload("res://unit_data/knight.tscn"),
 ]
 
 func spawn_unit() -> UnitController:
